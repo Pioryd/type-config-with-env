6e471eb8ebb6b8adc0447b62d46ca93866468dcf
Piotr Rydwański <pioryd123@gmail.com>
2022-12-15 13:09:17 +0100

    Add browsers support

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-import * as dotenv from "dotenv";
 import flatten from "flat";
 import _ from "lodash";
-
-// see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config();
 
 interface Options {
   preText?: string;
